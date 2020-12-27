dac8adfdbd8b6b9a624dd26ba8cd9fa63c1f84d7
Alan_Chok <alchok@live.com>
2020-12-26 18:52:43 -0700

    changes made

--- aboutme/aboutme.js
+++ aboutme/aboutme.js
@@ -2,6 +2,17 @@ let controller;
 let slideScene;
 let pageScene;
 
+const menu = document.querySelector("#mobile-menu");
+const menuLinks = document.querySelector(".navbar_menu");
+const navLogo = document.querySelector("#navbar_logo");
+
+const mobileMenu = () => {
+  menu.classList.toggle("is-active");
+  menuLinks.classList.toggle("active");
+};
+
+menu.addEventListener("click", mobileMenu);
+
 function animateSlides() {
   //Init Controller
   controller = new ScrollMagic.Controller();
